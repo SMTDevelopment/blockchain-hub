e3c787c0bc68af3529b1346ef34ad4a54e3520da
toanhuynh <toan.huynh@team.enouvo.com>
2023-03-03 10:29:26 +0700

    init: set up base

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# blockchain-hub
+# Enouvo Blockchain Hub
+
+Enouvo Blockchain Hub is built using [Docusaurus 2](https://docusaurus.io/) with `npm`. Static content hosting `vercel` service.
+
+## Local Development
+
+To set up the website locally:
+
+- Install dependencies using
+  ```
+  $ yarn
+  ```
+- Start the local development server and opens up a browser window
+  ```
+  $ yarn start
+  ```
+
+## Contributions
+
+If you are looking to make your contribution to this project, please follow the steps below.
+
+### Clone the repository
+
+Go to your Github account, open the repository, click on the code button and then click the `copy to clipboard` icon.
+
+### Create the branch
+
+- Checkout new branch from `main` branch.
+- Branch name convention: <`type`>/<`purpose`>. For example: research/polkadot-account, fix/update-ui
+
+### Commit those changes
+
+- Commit convention: <`type`><`(purpose)`>:<`description`>
+- Example: research(polkadot-account): summary information in vietnamese
+
+### Push and Submit changes for review
+
+- Push your changes
+- Go to `Pull requests` tab in repository in Github, click on `Compare & pull request` button.
+- Changing the branch range and destination repository
+- Type a title and description for your pull request
+- Submit pull request
---
+++ docusaurus.config.js
@@ -0,0 +1,138 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ENOUVO | Blockchain Team',
+  tagline: 'Development',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/enouvo-icon.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'enouvo', // Usually your GitHub org/user name.
+  projectName: 'blockchain-hub', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Enouvo Blockchain Hub',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/enouvo-logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Research',
+          },
+          // { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/Enouvo',
+            // label: "GitHub",
+            className: 'header-link-icon header-github-link',
+            'aria-label': 'GitHub repository',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Research',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              // {
+              //   label: 'Stack Overflow',
+              //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              // },
+              {
+                label: 'GitHub »',
+                href: 'https://github.com/Enouvo',
+              },
+              // {
+              //   label: 'Discord',
+              //   href: 'https://discordapp.com/invite/docusaurus',
+              // },
+              // {
+              //   label: 'Twitter',
+              //   href: 'https://twitter.com/docusaurus',
+              // },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
+              {
+                label: 'Enouvo IT Solutions',
+                href: 'https://enouvo.com/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Enouvo.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
